d374c4502f1fd3bfa62e2644a1ac14cd3c9271fd
Gbex <gabriel.mocellin204@gmail.com>
2023-03-18 10:34:42 -0300

    Acordeao funcionando

---
+++ home/js/acordeao.js
@@ -0,0 +1,52 @@
+/* Acordeao Funcionando */
+  const detailsTop = document.getElementById('details-top');
+  const detailsMid = document.getElementById('details-mid');
+  const detailsBottom = document.getElementById('details-bottom');
+
+  const summaryTop = detailsTop.querySelector('summary');
+  const summaryMid = detailsMid.querySelector('summary');
+  const summaryBottom = detailsBottom.querySelector('summary');
+
+  
+  detailsTop.removeAttribute('open'); // remove o atributo "open" inicialmente
+  detailsMid.removeAttribute('open'); // remove o atributo "open" inicialmente
+  detailsBottom.removeAttribute('open'); // remove o atributo "open" inicialmente
+  
+  summaryTop.addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão.
+    if (detailsMid.hasAttribute('open') || detailsBottom.hasAttribute('open')) {
+        detailsMid.removeAttribute('open'); //fechar os que já estão abertos
+        detailsBottom.removeAttribute('open');
+    }
+    if (detailsTop.hasAttribute('open')) {
+        detailsTop.removeAttribute('open', '');
+    } else{
+        detailsTop.setAttribute('open', '');
+    }
+  });
+
+  summaryMid.addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão.
+    if (detailsTop.hasAttribute('open') || detailsBottom.hasAttribute('open')) {
+        detailsTop.removeAttribute('open'); //fechar os que já estão abertos
+        detailsBottom.removeAttribute('open'); //fechar os que já estão abertos
+    }
+    if (detailsMid.hasAttribute('open')) {
+        detailsMid.removeAttribute('open', '');
+    } else{
+        detailsMid.setAttribute('open', '');
+    }
+  });
+
+  summaryBottom.addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão.
+    if (detailsTop.hasAttribute('open') || detailsMid.hasAttribute('open')) {
+        detailsTop.removeAttribute('open'); //fechar os que já estão abertos
+        detailsMid.removeAttribute('open'); //fechar os que já estão abertos
+    }
+    if (detailsBottom.hasAttribute('open')) {
+        detailsBottom.removeAttribute('open', '');
+    } else{
+        detailsBottom.setAttribute('open', '');
+    }
+  });
--- home/js/script.js
+++ home/js/script.js
@@ -83,18 +83,3 @@ document.addEventListener('DOMContentLoaded', function(event){
     dialogDiv.classList.remove('static');
     dialogDiv.classList.add('hidden');
   });
-
-  // Acordeao
-  const myDetails = document.getElementById('my-details');
-  const mySummary = myDetails.querySelector('summary');
-  var openS = false;
-  
-  myDetails.removeAttribute('open'); // remove o atributo "open" inicialmente
-  
-  mySummary.addEventListener('click', function() {
-    if (myDetails.hasAttribute('open')) {
-      myDetails.removeAttribute('open');
-    } else {
-      myDetails.setAttribute('open', '');
-    }
-  });
