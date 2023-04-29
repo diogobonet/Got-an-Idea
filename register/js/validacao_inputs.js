28133d54933a77df30886f96731c03ca76b3a49d
Gbex <gabriel.mocellin204@gmail.com>
2023-04-29 19:19:25 -0300

    Alteracoes regex + comecando conexao DB

--- register/js/validacao_inputs.js
+++ register/js/validacao_inputs.js
@@ -14,48 +14,83 @@ function ativar_notificacao(msg){
     setTimeout(()=>{
         fundo_not.classList.remove('visible');
         fundo_not.classList.add('hide');
-    }, 9000);
+    }, 9000); // Timer para remover a visibilidade da notificacao ao terminar o timer visivel
 }
 
 function validar_inputs(){
+
+    let formulario = document.getElementById('form_registro');
 
     let nome = document.querySelector('.info-nome');
     let sobrenome = document.querySelector('.info-sobrenome');
     let apelido = document.querySelector('.info-apelido');
     let email = document.querySelector('.info-email');
     let senha = document.querySelector('.info-senha');
+    let confirmar = document.querySelector('.info-confirmar');
     
     const regex_nome = /^[A-Z]{1}[a-z]{1,24}$/;
     const regex_senha = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[\!\@\#\$\%\¨\&\*\(\)\<\>\"\']).{8,16}$/;
+    const regex_email = /^\w{1,}\@\w{1,}\.\w{1,}(\.\w{1,})?$/
+    const regex_apelido = /^[\w\_]{3,25}$/
 
     if(nome.value.length>0 && sobrenome.value.length>0 && apelido.value.length>0 && email.value.length>0 && senha.value.length>0){
         let string_erros = ''; // Erros que serão colocados na notificação
+        let acertos = 0;
 
         if(regex_nome.test(nome.value)){
             nome.classList.remove('input_errado');
+            acertos+=1; /* com essa variavel será possível verificar se todas as condições foram atendidas */ 
         }else{
             nome.classList.add('input_errado');
-            string_erros += 'Nome: entre 2e25 caracteres e começar com maiuscula';
+            string_erros += 'Nome: entre 2&25 caracteres e começar com maiúscula';
         }
         
         if(regex_nome.test(sobrenome.value)){
             sobrenome.classList.remove('input_errado');
+            acertos+=1; /* com essa variavel será possível verificar se todas as condições foram atendidas */ 
         }else{
             sobrenome.classList.add('input_errado');
-            string_erros += '\nSobrenome: entre 2e25 caracteres e começar com maiuscula';
+            string_erros += 'Sobrenome: entre 2&25 caracteres e começar com maiúscula';
         }
+
+        if(regex_email.test(email.value)){
+            acertos+=1; /* com essa variavel será possível verificar se todas as condições foram atendidas */ 
+            email.classList.remove('input_errado');
+        }else{
+            email.classList.add('input_errado');
+            string_erros += 'Email inválido!';
+        }
+
+        if(regex_apelido.test(apelido.value)){
+            acertos+=1; /* com essa variavel será possível verificar se todas as condições foram atendidas */ 
+            apelido.classList.remove('input_errado');
+        }else{
+            apelido.classList.add('input_errado');
+            string_erros += 'Apelido: Apenas underlines,letras e números';
+        } 
         
         if(regex_senha.test(senha.value)){ // Teste da senha (Deve conter 8 caracteres incluindo Maiúsculas, mínusculas, números e especiais)
+            acertos+=1; /* com essa variavel será possível verificar se todas as condições foram atendidas */ 
             senha.classList.remove('input_errado');
         }else{
             senha.classList.add('input_errado');
-            string_erros += '\nSenha: entre 8 e 16 caracteres e ao menos 1 maiuscula e 1 especial';
+            string_erros += 'Senha: entre 8 e 16 caracteres e ao menos 1 maiúscula e 1 especial';
         }
         
-        ativar_notificacao(string_erros);
+        if(string_erros != ''){
+            ativar_notificacao(string_erros);
+        }
+
+        if(acertos == 5 && (senha.value == confirmar.value)){
+            formulario.submit();
+        }else{
+            if(senha.value != confirmar.value){
+                ativar_notificacao("As senhas não coincidem... Tente novamente!")
+            }
+        }
         
     }else{
-        alert('Um dos campos está vazio!\npor favor preencha-o!');
+        ativar_notificacao("Um dos campos está vazio... Por favor preencha-o!");
     }
 }
 
