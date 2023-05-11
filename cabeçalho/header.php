2359980a0864b01fda150625a832e9545dfd93b5
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-05-11 18:55:30 -0300

    Header

---
+++ cabeçalho/header.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+?>
+    <header>
+        <img src="../img/logowhite.png" alt="">
+        <input type="search" name="" id="">
+        <a href="">Encontre Ideias</a>
+        <a href="">Conectado</a>
+        <a href="">Ranking</a>
+        <img src="../edit-page/img/messi.jpeg" alt="">
+    </header>
